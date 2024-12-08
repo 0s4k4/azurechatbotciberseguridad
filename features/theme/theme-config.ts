c49731245eb641a8f8d507300e2086b1857ff2ad
0s4k4 <soulbosss@gmail.com>
2024-12-08 16:27:47 -0600

    se modifican archivos

--- features/theme/theme-config.ts
+++ features/theme/theme-config.ts
@@ -1,5 +1,6 @@
-export const AI_NAME = "Azure Chat";
-export const AI_DESCRIPTION = "Azure Chat is a friendly AI assistant.";
+export const AI_NAME = "Ciberto";
+//export const AI_DESCRIPTION = "Azure Chat is a friendly AI assistant.";
+export const AI_DESCRIPTION = "ciberto tu compa y asistente AI para ciberseguridad.";
 export const CHAT_DEFAULT_PERSONA = AI_NAME + " default";
 
 export const CHAT_DEFAULT_SYSTEM_PROMPT = `You are a friendly ${AI_NAME} AI assistant. You must always return in markdown format.
